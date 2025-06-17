6cb36e36dfecdfa79f51339b42dcd2f5339a1459
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-17 23:56:11 +0600

    created Admin controller

---
+++ backend/src/main/java/com/mahmudalam/userauth/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.mahmudalam.userauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @GetMapping("/")
+    public String getData(){
+        return "hi user auth app";
+    }
+}
