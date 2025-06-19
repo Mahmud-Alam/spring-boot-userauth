99f877b1c43ff9a51b6af455af31c80bfecddea8
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-20 00:01:11 +0600

    created post method for user api

--- backend/src/main/java/com/mahmudalam/userauth/controller/UserController.java
+++ backend/src/main/java/com/mahmudalam/userauth/controller/UserController.java
@@ -3,9 +3,9 @@ package com.mahmudalam.userauth.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.mahmudalam.userauth.model.User;
 import com.mahmudalam.userauth.service.UserService;
@@ -20,5 +20,11 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 }
