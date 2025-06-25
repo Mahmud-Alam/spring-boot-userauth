7424b479f943b21009079b5c4255bf18ef96943a
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-25 21:54:27 +0600

    add modiify and transactional into repo file

--- backend/src/main/java/com/mahmudalam/userauth/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/mahmudalam/userauth/repository/RefreshTokenRepository.java
@@ -3,7 +3,9 @@ package com.mahmudalam.userauth.repository;
 import com.mahmudalam.userauth.model.RefreshToken;
 import com.mahmudalam.userauth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -12,5 +14,7 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     Optional<RefreshToken> findByToken(String token);
 
+    @Modifying
+    @Transactional
     void deleteByUser(User user);
 }
