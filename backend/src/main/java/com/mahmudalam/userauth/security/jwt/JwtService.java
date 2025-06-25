ed5ad9265c48df685eddddec2b5b7d848ce83633
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-25 22:23:43 +0600

    settings for access and refresh token

--- backend/src/main/java/com/mahmudalam/userauth/security/jwt/JwtService.java
+++ backend/src/main/java/com/mahmudalam/userauth/security/jwt/JwtService.java
@@ -1,27 +1,39 @@
 package com.mahmudalam.userauth.security.jwt;
 
+import com.mahmudalam.userauth.model.User;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+@Data
 @Service
 public class JwtService {
-    private final String secretKey;
+//    private final String secretKey;
 
-    public JwtService() {
+    @Value("${jwt.access.secret}")
+    private String accessSecret;
+
+    @Value("${jwt.refresh.secret}")
+    private String refreshSecret;
+
+    @Value("${jwt.access.expiry}")
+    private Long accessTokenValidity;
+
+    @Value("${jwt.refresh.expiry}")
+    private Long refreshTokenValidity;
+
+    /*
+    public JwtService(){
         try {
             KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
             SecretKey sk = keyGen.generateKey();
@@ -33,8 +45,7 @@ public class JwtService {
 
     public String generateToken(String username) {
         Map<String, Object> claims = new HashMap<>();
-        return Jwts
-                .builder()
+        return Jwts.builder()
                 .claims()
                 .add(claims)
                 .subject(username)
@@ -44,11 +55,75 @@ public class JwtService {
                 .signWith(getKey())
                 .compact();
     }
+    * */
 
+    public String generateAccessToken(User user) {
+        Map<String, Object> claims = Map.of("role", user.getRole().name());
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + accessTokenValidity))
+                .and()
+                .signWith(getAccessKey())
+                .compact();
+    }
+
+    public String generateRefreshToken(User user) {
+        return Jwts.builder()
+                .subject(user.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + refreshTokenValidity))
+                .signWith(getRefreshKey())
+                .compact();
+    }
+
+    public String extractUsernameFromRefresh(String token) {
+        return extractClaim(token, Claims::getSubject, getRefreshKey());
+    }
+
+    public boolean isRefreshTokenValid(String token) {
+        return !isTokenExpired(token, getRefreshKey());
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> resolver, SecretKey key) {
+        Claims claims = Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
+        return resolver.apply(claims);
+    }
+
+    private boolean isTokenExpired(String token, SecretKey key) {
+        Date expiry = extractClaim(token, Claims::getExpiration, key);
+        return expiry.before(new Date());
+    }
+
+    private SecretKey getAccessKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(accessSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private SecretKey getRefreshKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(refreshSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // extract username from access token
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject, getAccessKey());
+    }
+
+    // validate access token validity and ownership
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token, getAccessKey()));
+    }
+
+    /*
     private SecretKey getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
+
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -83,4 +158,6 @@ public class JwtService {
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
+
+    * */
 }
--- backend/src/main/java/com/mahmudalam/userauth/service/AuthService.java
+++ backend/src/main/java/com/mahmudalam/userauth/service/AuthService.java
@@ -3,7 +3,9 @@ package com.mahmudalam.userauth.service;
 import com.mahmudalam.userauth.dto.request.LoginRequest;
 import com.mahmudalam.userauth.dto.request.RegisterRequest;
 import com.mahmudalam.userauth.dto.response.AuthResponse;
+import com.mahmudalam.userauth.model.RefreshToken;
 import com.mahmudalam.userauth.model.User;
+import com.mahmudalam.userauth.repository.RefreshTokenRepository;
 import com.mahmudalam.userauth.repository.UserRepository;
 import com.mahmudalam.userauth.security.jwt.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +15,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.Optional;
 
 @Service
+@Transactional
 public class AuthService {
 
     @Autowired
@@ -29,49 +36,89 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
+
     public AuthResponse login(LoginRequest request) {
         try {
-            String requestedUsername = request.getUsername().toLowerCase();
+            String requestUsername = request.getUsername().toLowerCase();
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
-                            requestedUsername, request.getPassword()));
+                            requestUsername, request.getPassword()));
 
             if (authentication.isAuthenticated()) {
-                String token = jwtService.generateToken(requestedUsername);
-                return new AuthResponse(true, token, null);
+                User user = userRepository.findByUsername(requestUsername);
+
+                // Delete existing refresh tokens for user
+                refreshTokenRepository.deleteByUser(user);
+                refreshTokenRepository.flush(); // <-- force delete to DB
+
+                String accessToken = jwtService.generateAccessToken(user);
+                String refreshTokenStr = jwtService.generateRefreshToken(user);
+
+                RefreshToken refreshToken = RefreshToken.builder()
+                        .token(refreshTokenStr)
+                        .user(user)
+                        .expiryDate(new Date(System.currentTimeMillis() + jwtService.getRefreshTokenValidity()))
+                        .build();
+
+                refreshTokenRepository.save(refreshToken);
+
+                return new AuthResponse(true, accessToken, refreshTokenStr, null);
             }
-        } catch (AuthenticationException e) {
-            return new AuthResponse(false, null, "Invalid credentials.");
+        } catch (AuthenticationException e){
+            return new AuthResponse(false,null, null,"Invalid credentials.");
         }
 
-        return new AuthResponse(false, null, "Authentication failed.");
+        return new AuthResponse(false, null, null, "Authentication failed.");
     }
 
     public AuthResponse register(RegisterRequest request) {
         try {
-            String requestedUsername = request.getUsername().toLowerCase();
-            String requestedEmail = request.getEmail().toLowerCase();
-            if (userRepository.existsByUsername(requestedUsername)) {
-                return new AuthResponse(false, null, "Username already exists.");
+            String requestUsername = request.getUsername().toLowerCase();
+            String requestEmail = request.getEmail().toLowerCase();
+            if(userRepository.existsByUsername(requestUsername)){
+                return new AuthResponse(false, null, null, "Username already exists.");
             }
-            if (userRepository.existsByEmail(requestedEmail)) {
-                return new AuthResponse(false, null, "Email already exists.");
+            if(userRepository.existsByEmail(requestEmail)){
+                return new AuthResponse(false, null, null, "Email already exists.");
             }
 
             User user = User.builder()
-                    .username(requestedUsername)
-                    .email(requestedEmail)
+                    .username(requestUsername)
+                    .email(requestEmail)
                     .password(passwordEncoder.encode(request.getPassword()))
                     .build();
 
             userRepository.save(user);
 
-            String token = jwtService.generateToken(requestedUsername);
+            String accessToken = jwtService.generateAccessToken(user);
+            String refreshTokenStr = jwtService.generateRefreshToken(user);
 
-            return new AuthResponse(true, token, null);
+            RefreshToken refreshToken = RefreshToken.builder()
+                    .token(refreshTokenStr)
+                    .user(user)
+                    .expiryDate(new Date(System.currentTimeMillis() + jwtService.getRefreshTokenValidity()))
+                    .build();
+
+            refreshTokenRepository.save(refreshToken);
+
+            return new AuthResponse(true, accessToken, refreshTokenStr, null);
 
         } catch (Exception e) {
-            return new AuthResponse(false, null, "User registration failed.");
+            return new AuthResponse(false, null, null, "User registration failed.");
         }
+    }
+
+    public AuthResponse refreshToken(String refreshTokenStr) {
+        Optional<RefreshToken> tokenOpt = refreshTokenRepository.findByToken(refreshTokenStr);
+
+        if (tokenOpt.isEmpty() || !jwtService.isRefreshTokenValid(refreshTokenStr)) {
+            return new AuthResponse(false, null, null, "Invalid or expired refresh token.");
+        }
+
+        User user = tokenOpt.get().getUser();
+        String newAccessToken = jwtService.generateAccessToken(user);
+        return new AuthResponse(true, newAccessToken, null, null);
     }
 }
