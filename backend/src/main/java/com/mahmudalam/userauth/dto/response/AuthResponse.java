df95591953f1d2ff48c61eb930cbdc291e46fb02
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-25 21:52:27 +0600

    add new property in auth response

--- backend/src/main/java/com/mahmudalam/userauth/dto/response/AuthResponse.java
+++ backend/src/main/java/com/mahmudalam/userauth/dto/response/AuthResponse.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class AuthResponse {
     private boolean success;
-    private String token;
+    private String accessToken;
+    private String refreshToken;
     private String error;
 }
