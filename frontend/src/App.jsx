122917b60778727ffbbdde3bf2f5584b1281af77
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-28 23:01:40 +0600

    update routings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,55 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import { ToastContainer } from "react-toastify";
+import Register from "./pages/Register";
+import PrivateRoute from "./routes/PrivateRoute";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import { useAppContext } from "./contexts/AppContext";
+import Layout from "./components/Layout";
 
-export default App
+const App = () => {
+  const { accessToken } = useAppContext();
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            accessToken ? (
+              <Navigate to="/profile" replace />
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute role="ADMIN">
+              <Layout>
+                <Dashboard />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <Profile />
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
