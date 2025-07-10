6c974292ce49d023ea675438984a00f709e652ed
Mahmud-Alam <official.mahmud23@gmail.com>
2025-07-10 23:28:32 +0600

    add swagger endpoints permission

--- backend/src/main/java/com/mahmudalam/userauth/security/config/SecurityConfig.java
+++ backend/src/main/java/com/mahmudalam/userauth/security/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated())
 // using JWT            .httpBasic(Customizer.withDefaults())
                 .exceptionHandling(e -> e
